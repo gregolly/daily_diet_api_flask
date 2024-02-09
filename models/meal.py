da674ce7cc80f2bf6b2aff8b5162440749b042d7
gregolly <gregollyff@hotmail.com>
2024-02-09 18:52:15 -0400

    feat: table meal

---
+++ models/meal.py
@@ -0,0 +1,14 @@
+from database import db
+from datetime import datetime
+
+class Meal(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200))
+    date = db.Column(db.DateTime(), nullable=False)
+    hour = db.Column(db.DateTime(), nullable=False)
+    onDiet = db.Column(db.Boolean(), nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", back_populates="meals")
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime(), default=datetime.utcnow, nullable=False)
