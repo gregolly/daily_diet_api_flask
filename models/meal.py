21d2772b44b7f8a030ef447c4134d2f6b8399c81
gregolly <gregollyff@hotmail.com>
2024-02-09 19:26:11 -0400

    feat: remove usermixin

--- models/meal.py
+++ models/meal.py
@@ -1,11 +1,9 @@
 from database import db
 from datetime import datetime
-from flask_login import UserMixin
-
-class Meal(db.Model, UserMixin):
+class Meal(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(200))
+    description = db.Column(db.Text)
     date = db.Column(db.DateTime(), nullable=False)
     hour = db.Column(db.DateTime(), nullable=False)
     onDiet = db.Column(db.Boolean(), nullable=False)
