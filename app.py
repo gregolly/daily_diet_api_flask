37cfde2b3d5b98c4058e05a2b269e1dcfdc9e716
gregolly <gregollyff@hotmail.com>
2024-02-10 10:04:31 -0400

    feat: change

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import db
-from flask import request, jsonify
 from database import db
 from models.user import User
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
