e0ab66aa620dde479ff9a061ebe268f67061292d
gregolly <gregollyff@hotmail.com>
2024-02-10 10:23:21 -0400

    removing comments

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'user_bp.login'  # Change 'login' to 'user_bp.login'
+login_manager.login_view = 'user_bp.login'
 
-app.register_blueprint(user_bp, url_prefix='/user')  # This sets a prefix for all routes in user_bp
+app.register_blueprint(user_bp, url_prefix='/user')
 
 if __name__ == '__main__':
     app.run(debug=True)
