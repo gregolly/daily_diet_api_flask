3fbd4c6776297a0246b47a0fac675313aee19c09
gregolly <gregollyff@hotmail.com>
2024-02-10 20:23:02 -0400

    feat: add routes to get all meals, update and delete

--- routes/meal_routes.py
+++ routes/meal_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_required
 from database import db
 from models.meal import Meal
+from models.user import User
 from datetime import datetime
 
 meal_bp = Blueprint('meal_bp', __name__)
@@ -57,4 +58,59 @@ def get_meal(id):
         }
     
     return jsonify({"message": "Meal not found"}), 404
+
+@meal_bp.route("/all/<int:id>", methods=['GET'])
+@login_required
+def get_meals(id):
+    user = User.query.get(id)
+    meals = Meal.query.all()
+
+    if user and current_user.is_authenticated:
+        meals = Meal.query.filter_by(user_id=id).all()
+
+        meal_list = []
+        for meal in meals:
+            meal_data = {
+                "title": meal.title,
+                "description": meal.description,
+                "date": meal.date,
+                "hour": meal.hour,
+                "onDiet": meal.onDiet
+            }
+            meal_list.append(meal_data)
+        return jsonify(meal_list)
+
+    return jsonify({"message": "unauthorized"})
+
+@meal_bp.route("/<int:id>", methods=['PUT'])
+@login_required
+def update_meal(id):
+    data = request.json
+    meal = Meal.query.get(id)
+
+    if not current_user.is_authenticated:
+        return jsonify({"message": "You cannot change meal of another user"}), 403
+    
+    if meal and data.get("title"):
+        meal.title = data.get("title")
+        meal.description = data.get("description")
+        meal.date = datetime.strptime(data.get("date"), "%d/%m/%Y") if data.get("date") else meal.date
+        meal.hour = datetime.strptime(data.get("hour"), "%H:%M").strftime("%H:%M") if data.get("hour") else meal.hour
+        meal.onDiet = data.get("onDiet")
+        db.session.commit()
+        return jsonify({"message": f"Meal {id} has been updated successful"})
+    
+    return jsonify({"message": "Meal not found"}), 404 
+    
+@meal_bp.route("/<int:id>", methods=['DELETE'])
+@login_required
+def delete_meal(id):
+    meal = Meal.query.get(id)
+    
+    if meal:
+        db.session.delete(meal)
+        db.session.commit()
+        return jsonify({"message": f"Meal {id} has been deleted successful"})
+    
+    return jsonify({"message": "User not found"}), 404
 
